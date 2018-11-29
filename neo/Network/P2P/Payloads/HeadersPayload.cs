f1664abecf169d40c97edbb5a8a810d3270ea07d
shawn-yun <yangxioayun@neo.org>
2018-11-29 17:52:43 +0800

    add payload

--- neo/Network/P2P/Payloads/HeadersPayload.cs
+++ neo/Network/P2P/Payloads/HeadersPayload.cs
@@ -5,14 +5,31 @@ using System.Linq;
 
 namespace Neo.Network.P2P.Payloads
 {
+    /// <summary>
+    /// 区块头负载类，定义了区块头的结构等
+    /// </summary>
     public class HeadersPayload : ISerializable
     {
+        /// <summary>
+        /// 最大区块头数量
+        /// </summary>
         public const int MaxHeadersCount = 2000;
 
+        /// <summary>
+        /// 区块头数组
+        /// </summary>
         public Header[] Headers;
 
+        /// <summary>
+        /// 区块头数组的大小
+        /// </summary>
         public int Size => Headers.GetVarSize();
 
+        /// <summary>
+        /// 根据可枚举区块头集合创建一个区块头负载
+        /// </summary>
+        /// <param name="headers">可枚举区块头集合</param>
+        /// <returns>创建的区块头负载</returns>
         public static HeadersPayload Create(IEnumerable<Header> headers)
         {
             return new HeadersPayload
